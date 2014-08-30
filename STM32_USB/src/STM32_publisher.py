cd035c082d628097b4d042e7ef1319253afd5b65
Andres Tosca <atr.andres@gmail.com>
2014-08-30 09:29:15 -0700

    Adding a new ROS package for the communication between the STM32 and the PC. Documentation is pending

---
+++ STM32_USB/src/STM32_publisher.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import serial
+from struct import unpack
+import rospy
+import sys
+from STM32_USB import encoder, battery, compass
+
+port_name = sys.argv[1]
+serial = serial.Serial(port_name)
+
+def talker():
+	pubEncoder = rospy.Publisher('Encoder', encoder) # Topic with name Encoder and message encoder (int32)
+	pubBattery = rospy.Publisher('Battery', battery) # Topic with name Battery and message battery (int32)
+	pubCompass = rospy.Publisher('Compass', compass) # Topic with name Compass and message compass (float32)
+	rospy.init_node('STM32_USB') # Node with name STM32_USB
+	r = rospy.Rate(10) #Hz
+	while not rospy.is_shutdown():
+		data = serial.read(14)
+		structData = unpack('!cid',data)
+		if structData[0] == 'a':
+			pubEncoder.publish(structData[1])
+		elif structData[0] == 'b':
+			pubBattery.publish(structData[1])
+		elif structData[0] == 'c':
+			pubCompass.publish(structData[2])
+		r.sleep()
+
+if __name__ == '__main__':
+	try: 
+		talker()
+	except rospy.ROSInterruptException: pass
+
+
+
+
+
