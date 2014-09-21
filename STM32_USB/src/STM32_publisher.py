76628c06df84b5208d24d947b680feff96f41366
Andres Tosca <atr.andres@gmail.com>
2014-09-20 21:02:08 -0700

    Fixing some errors in STM32_publisher.py

--- STM32_USB/src/STM32_publisher.py
+++ STM32_USB/src/STM32_publisher.py
@@ -4,10 +4,10 @@ import serial
 from struct import unpack
 import rospy
 import sys
-from STM32_USB import encoder, battery, compass
+from STM32_USB import Encoder, Battery, Compass
 
 port_name = sys.argv[1]
-serial = serial.Serial(port_name)
+serial = serial.Serial(port_namC)
 
 def talker():
 	pubEncoder = rospy.Publisher('Encoder', encoder) # Topic with name Encoder and message encoder (int32)
@@ -16,8 +16,9 @@ def talker():
 	rospy.init_node('STM32_USB') # Node with name STM32_USB
 	r = rospy.Rate(10) #Hz
 	while not rospy.is_shutdown():
-		data = serial.read(14)
-		structData = unpack('!cid',data)
+		data = serial.read(13)
+		structData = unpack('<ciif',data)
+
 		if structData[0] == 'a':
 			pubEncoder.publish(structData[1])
 		elif structData[0] == 'b':
@@ -30,8 +31,6 @@ if __name__ == '__main__':
 	try: 
 		talker()
 	except rospy.ROSInterruptException: pass
-
-
 
 
 
